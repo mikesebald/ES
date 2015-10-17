08dc724cd3df1cb5a2eb313193082793ad8d2cf4
mikesebald <michael.sebald@gmail.com>
2015-10-17 17:21:48 +0200

    update

--- code/elastic.R
+++ code/elastic.R
@@ -6,6 +6,7 @@
 library(elastic)
 library(jsonlite)
 library(lubridate)
+library(stringr)
 
 connect()
 #shakespeare <- system.file("examples", "shakespeare_data.json", package = "elastic")
@@ -35,5 +36,11 @@ cat_indices(verbose = TRUE)
 vehicles <- read.csv("data//motor_vehicle_theft.csv", header = TRUE, stringsAsFactors = FALSE)
 
 x <- strptime(vehicles[1,1], "%m/%d/%Y %I:%M:%S %p", tz = "US/Michigan")
+vehicles$Date <- strptime(vehicles$Date, "%m/%d/%Y %I:%M:%S %p", tz = "US/Michigan")
+
+vehicles$Location <- gsub("\\(|\\)|\\s", "", vehicles$Location)
+vehicles$latitude <- str_split_fixed(vehicles$Location, ",", 2)[,1]
+vehicles$longitude <- str_split_fixed(vehicles$Location, ",", 2)[,2]
 
 
+View(vehicles)
